91a9181b6723e99fca8cd8031ff35cc5e68943a7
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-10-08 18:53:26 +0530

    fucntions

--- functions.py
+++ functions.py
@@ -1,3 +1,17 @@
 
+def send_loan_reminder(a, b):
+    msg = f'''
+    Dear {a},
 
-print('hello')
+    Kindly clear your pending dues of Rs.{b}!
+
+    Thanks
+    HDFC 
+    '''
+    print(msg)
+names = {'Arun': 20000, 'veera':40000, 'Charan':25000, 'Praneeth':30000}
+for i in names: #i=Arun
+   name = i
+   amount = names[i]
+   send_loan_reminder(name, amount)
+   print('----------')
